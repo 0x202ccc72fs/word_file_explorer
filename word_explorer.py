8ad88e417960277bc9106e7edae48b6caa4f932a
spynac <spynac@mailfence.com>
2025-01-07 09:32:54 +0100

    word_explorer.py

--- word_explorer.py
+++ word_explorer.py
@@ -1,8 +1,21 @@
-user_input = input("Enter Filename:".upper())
-fname      = open(user_input)
-for x in fname:
-        pos  = x.find("######")
+def word_explorer():
+   user_input = input("Enter Filename:".upper())
+   fname      = open(user_input)
+   for x in fname:
+        pos  = x.find("Admin")
         if pos  != -1:
-           pos1  = x[pos:].strip()
-           print(pos1)
-quit()
+         pos1  = x[pos:].strip()
+         print(pos1)
+
+def user_input():
+        print("1.Run Programm")
+        print("2.Quit Programm")
+        ent_input = input("Select 1 or 2 : ".strip())
+        for x in ent_input:
+            if   x == "1":
+                 word_explorer()
+            elif x == "2":
+                 print("Succesfull Closed!".upper()) 
+                 quit()
+
+user_input()
